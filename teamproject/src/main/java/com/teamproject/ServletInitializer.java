7ddecb36d227f47efceaac579fb92c42ade2945e
hjin2017 <hjin2015@gmail>
2021-05-22 17:40:22 +0900

    add face

--- teamproject/src/main/java/com/teamproject/ServletInitializer.java
+++ teamproject/src/main/java/com/teamproject/ServletInitializer.java
@@ -2,8 +2,7 @@ package com.teamproject;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-@ComponentScan(basePackageClasses =MainController.class) 
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
--- teamproject/src/main/java/com/teamproject/TeamprojectApplication.java
+++ teamproject/src/main/java/com/teamproject/TeamprojectApplication.java
@@ -2,8 +2,11 @@ package com.teamproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = MainController.class)
 public class TeamprojectApplication {
 
 	public static void main(String[] args) {

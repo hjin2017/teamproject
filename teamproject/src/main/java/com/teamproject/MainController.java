4c3da2b3909f142b2cda91ee388a78ef52e69b81
hjin2017 <hjin2015@gmail>
2021-05-28 10:00:41 +0900

    controller absorption

--- teamproject/src/main/java/com/teamproject/MainController.java
+++ teamproject/src/main/java/com/teamproject/MainController.java
@@ -1,10 +1,15 @@
 package com.teamproject;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
@@ -20,8 +25,47 @@ public class MainController {
 	
 	//김세미 end
 	//이현용
+	@Autowired
+	NaverOCRService ocrservice;
+	@Autowired
+	NaverVoiceService voiceservice;
+	//ocr
 	
-	//작업 공간
+	@RequestMapping("/ocrinput")
+	public ModelAndView ocrinput() {
+		File f = new File("C:/Users/Maple/Desktop/images"); 
+		String[] filelist = f.list();
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("filelist",filelist);
+		mv.setViewName("/lhy/ocrinput");
+		return mv;
+	}
+	@RequestMapping(value="/ocr",method=RequestMethod.POST)
+	public ModelAndView ocr(String image) { 
+		String result = ocrservice.test(image);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("ocrResult",result);
+		mv.setViewName("/lhy/ocr");
+		return mv;
+	}
+	//upload
+	@RequestMapping(value="/imgupload" , method = RequestMethod.POST)
+	@ResponseBody
+	public String uploadresult(MultipartFile file) throws IOException{
+		String filename = file.getOriginalFilename();
+		String savePath = "C:/Users/Maple/Desktop/images/";
+		File savefile = new File(savePath + filename);	
+		file.transferTo(savefile);
+		return "잘받았습니다"; 
+	}
+	/*
+	 * //tts
+	 * 
+	 * @RequestMapping("/tts")
+	 * 
+	 * @ResponseBody public String voice(String text , String speaker ) { String
+	 * result = voiceservice.test(text,speaker); return result;// 리턴 *.mp3 파일명 }
+	 */
 	
 	//이현용 end
 	//허진호
